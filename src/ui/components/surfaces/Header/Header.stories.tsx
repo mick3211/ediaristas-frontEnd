0e449ddb30ad1f494de1eb2f8df29fa0e776f3ec
Mickael <mickaelfelizardo2008@gmail.com>
2022-09-20 17:17:53 -0300

    user prop missing

--- src/ui/components/surfaces/Header/Header.stories.tsx
+++ src/ui/components/surfaces/Header/Header.stories.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import Header from './Header';
+import { UserInterface } from 'data/@types/UserInterface';
 
 export default {
     title: 'Surfaces/Header',
     component: Header,
 } as ComponentMeta<typeof Header>;
 
-const Template: ComponentStory<typeof Header> = args => <Header />;
+const Template: ComponentStory<typeof Header> = (args) => (
+    <Header user={{} as UserInterface} />
+);
 
 export const Default = Template.bind({});
 
